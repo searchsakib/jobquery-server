3ec064f0f30ef0fa270e7b1da3c26007a1d5a67d
searchsakib <searchsakib@gmail.com>
2023-11-08 00:23:50 +0600

    finding 1 data with mongoDB _id successful

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,13 @@ app.get('/', (req, res) => {
 app.get('/jobs', async (req, res) => {
   const cursor = jobsCollection.find();
   const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.get('/jobs/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await jobsCollection.findOne(query);
   res.send(result);
 });
 
